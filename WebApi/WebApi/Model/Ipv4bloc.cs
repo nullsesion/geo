49bb9c26fe8d8a5a56cebda67a1f20298352a3ec
nullsesion <nullsesion@yandex.ru>
2020-02-09 19:15:40 +0300

    add controller and model

---
+++ WebApi/WebApi/Model/Ipv4bloc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace loadMaxmind.Model
+{
+    public class Ipv4bloc
+    {
+        public Guid ID { get; set; }
+        public string Network { get; set; }
+        public string GeonameId { get; set; }
+        public string RegisteredCountryGeonameId { get; set; }
+        public string RepresentedCountryGeonameId { get; set; }
+        public string IsAnonymousProxy { get; set; }
+        public string IsSatelliteProvider { get; set; }
+        public UInt32 IpMax { get; set; }
+        public UInt32 IpMin { get; set; }
+    }
+}
