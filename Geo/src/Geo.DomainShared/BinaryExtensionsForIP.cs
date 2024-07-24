bf25e80dd09cf956ab97ff11aabe816d699b3e7e
nullsesion <nullsesion@yandex.ru>
2024-07-24 15:11:35 +0300

    refactor work with ip v4

---
+++ Geo/src/Geo.DomainShared/BinaryExtensionsForIP.cs
@@ -0,0 +1,107 @@
+﻿using System.Net;
+
+namespace Geo.DomainShared
+{
+	public static class BinaryExtensionsForIP
+	{
+		public static bool TryIpV4ToInt(this string ip, out UInt32 number)
+		{
+			number = 0;
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				number = BitConverter.ToUInt32(GetBytesFromIP(iPAddress));
+				return true;
+			}
+
+			return false;
+		}
+		public static bool TryIpV4ToInt(this string ip, out Int32 number)
+		{
+			number = 0;
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				number = BitConverter.ToInt32(GetBytesFromIP(iPAddress));
+				return true;
+			}
+
+			return false;
+		}
+		
+
+		public static bool TryIpV6ToInt(this string ip, out UInt128 number)
+		{
+			number = 0;
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				throw new NotImplementedException();
+				return true;
+			}
+
+			return false;
+		}
+		public static bool TryIpV6ToInt(this string ip, out Int128 number)
+		{
+			number = 0;
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				throw new NotImplementedException();
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool TryIpV4GetMaxMinViaMask(this string ip, int mask, out UInt32 max, out UInt32 min)
+		{
+			max = min = 0;
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				UInt32 bits = 0b_0000_0000_0000_0000_0000_0000_0000_0000;
+				UInt32 currentBit = 0b_0000_0000_0000_0000_0000_0000_0000_0001;
+				if (mask <= 32)
+				{
+					bits = bits | currentBit;
+					for (int i = 32; i > mask; i--)
+					{
+						bits = bits | currentBit;
+						currentBit = currentBit << 1;
+					}
+				}
+				UInt32 number = BitConverter.ToUInt32(GetBytesFromIP(iPAddress));
+				max = number | bits;
+				min = number & ~bits;
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool TryIpToBytes(this string ip, out byte[] bytes)
+		{
+			bytes = new byte[]{};
+			if (IPAddress.TryParse(ip, out IPAddress iPAddress))
+			{
+				bytes = GetBytesFromIP(iPAddress);
+				return true;
+			}
+
+			return false;
+		}
+		
+		public static UInt32 ToUInt32(this int n)
+		{
+			var bytes = BitConverter.GetBytes(n);
+			return BitConverter.ToUInt32(bytes);
+		}
+
+		private static byte[] GetBytesFromIP(IPAddress iPAddress)
+		{
+			byte[] bytesIP = iPAddress
+				.GetAddressBytes()
+				.Reverse()
+				.ToArray<Byte>();
+			return bytesIP;
+		}
+	}
+}
--- Geo/tests/Get.DomainShared.Tests/UnitIPV4.cs
+++ Geo/tests/Get.DomainShared.Tests/UnitIPV4.cs
@@ -1,4 +1,5 @@
 using Geo.DomainShared;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Get.DomainShared.Tests
 {
@@ -6,16 +7,58 @@ namespace Get.DomainShared.Tests
 	public class UnitIPV4
 	{
 		[TestMethod]
+		public void Try_Convert_IPV4_FromString_IntReturned()
+		{
+			//arrange
+			string IP = "104.16.196.14";
+
+			//act
+			if (IP.TryIpV4ToInt(out Int32 number))
+				Assert.AreEqual(0x_68_10_c4_0e, number);
+			else
+				Assert.Fail("an error occurred while parsing");
+		}
+
+		[TestMethod]
 		public void Try_Convert_IPV4_FromString_UIntReturned()
 		{
 			//arrange
-			string IP = "62.118.93.157";
+			string IP = "104.16.196.14";
 
 			//act
-			if (IP.TryIpV4ToInt32(out Int32 number))
-				Assert.AreEqual(1047944605, number);
+			if (IP.TryIpV4ToInt(out UInt32 number))
+				Assert.AreEqual<UInt32>(0x_68_10_c4_0e, number);
 			else
-				Assert.Fail("bad parse");
+				Assert.Fail("an error occurred while parsing");
+		}
+
+		[TestMethod]
+		public void Try_Convert_IPV4_FromString_LessZeroReturned()
+		{
+			//arrange
+			string IP = "150.16.196.14";
+
+			//act
+			if (IP.TryIpV4ToInt(out Int32 number))
+				Assert.IsTrue(number < 0);
+			else
+				Assert.Fail("an error occurred while parsing");
+		}
+
+		[TestMethod]
+		public void Try_IPV4_GetMaxMin_MaxMinReturned()
+		{
+			//1.0.1.0/24
+			string IP = "1.0.1.0"; //1000000000000000100000000
+			uint mask = 24; //0000000000000000011111111
+			if (IP.TryIpV4GetMaxMinViaMask(24, out uint max, out uint min))
+			{
+				Assert.AreEqual<UInt32>(0b_1000000000000000111111111, max);
+				Assert.AreEqual<UInt32>(0b_1000000000000000100000000, min);
+			}
+			else
+				Assert.Fail("an error occurred while parsing");
+
 		}
 	}
 }
